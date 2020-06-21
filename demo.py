76bff66e007ad32acb5e9784458b0131b84102c7
Kristof <kristof.kovacs@telenav.com>
2020-06-22 01:44:19 +0300

    new version

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 from kcu import kjson, strings
+import string
 
 from reddit_scraper.reddit_scraper import RedditScraper
 
@@ -7,7 +8,7 @@ posts = RedditScraper.get_posts('askreddit', max_count=10)
 print(len(posts))
 
 id_ = 'hckkcp'
-post = RedditScraper.get_post(id_, comments_min_score=10000)
+post = RedditScraper.get_post(id_, comments_min_score=5000)
 
 j = post.json
 del j['post_dict']
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="reddit_scraper",
-    version="0.0.2",
+    version="0.0.3",
     author="Kristof",
     description="reddit_scraper",
     long_description=long_description,
