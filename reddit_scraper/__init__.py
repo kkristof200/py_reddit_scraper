aafd099a76861df6665d5d826adec7d154e9c32a
Kristof <kristof.kovacs@telenav.com>
2020-06-23 13:21:39 +0300

    new version

--- reddit_scraper/__init__.py
+++ reddit_scraper/__init__.py
@@ -1,5 +1,9 @@
 from .reddit_scraper import RedditScraper
 
-from .models.post import Post
-from .models.sorting_type import SortingType
 from .models.time_interval import TimeInterval
+from .models.sorting_type import SortingType
+from .models.post import Post
+from .models.comment import Comment
+from .models.image import Image
+from .models.video import Video
+from .models.post_type import PostType
